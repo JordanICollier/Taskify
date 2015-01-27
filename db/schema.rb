e88c9c6b79969d912b7ca303fda3f738f8c6cd38
Jordan Collier <jordanicollier@gmail.com>
2015-01-27 12:27:21 -0700

    add complete checkbox on edit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150127021242) do
+ActiveRecord::Schema.define(version: 20150127192011) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20150127021242) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.date     "due_date"
+    t.boolean  "complete"
   end
 
 end
