d67fdb2172b186acbb829aa67f8712aff79f9b1c
Jordan Collier <jordanicollier@gmail.com>
2015-02-03 18:32:35 -0700

    add users controller, model, index and new. Also add user to footer

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,46 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+    @full
+  end
+
+  def show
+  end
+
+  def new
+    @user = User.new
+
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to users_path, notice: 'User was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @task = User.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email)
+    end
+
+end
