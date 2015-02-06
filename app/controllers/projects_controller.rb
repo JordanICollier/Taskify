c3bf38b5d6c12f7b0c33efae5df9bb231bf5b305
Jordan Collier <jordanicollier@gmail.com>
2015-02-05 17:12:10 -0700

    Add crud users

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,51 @@
+class ProjectsController < ApplicationController
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @projects = Project.all
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to @project, notice: "Project was successfully created"
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+    def update
+      if @project.update(project_params)
+        redirect_to @project, notice: 'Project was successfully updated.'
+      else
+        render :new
+      end
+    end
+
+    def destroy
+      if @project.destroy
+        redirect_to projects_path, notice: 'Project was successfully deleted.'
+      end
+    end
+
+
+    private
+
+    def set_project
+      @project = Project.find(params[:id])
+    end
+
+    def project_params
+      params.require(:project).permit(:name)
+    end
+  end
