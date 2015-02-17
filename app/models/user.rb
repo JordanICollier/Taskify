fc73efdb8613023180cd7eaff34a672df1cd0f33
Jordan Collier <jordanicollier@gmail.com>
2015-02-17 15:11:31 -0700

    add uniqueness to user email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ActiveRecord::Base
   def full_name
     "#{first_name} #{last_name}"
   end
-
-  validates :first_name, :last_name, :email, presence: true 
+  validates :email, presence: true, uniqueness: true
+  validates :first_name, :last_name, presence: true
 end
