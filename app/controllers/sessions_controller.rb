bf1f79a144d4755b9417239f63b871ab1d845d75
Jordan Collier <jordanicollier@gmail.com>
2015-02-19 15:25:54 -0700

    fix flash alert on sign up and sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,8 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to root_path, notice: 'Welcome back, stranger!'
     else
-      redirect_to login_path, alert: "Username / password combination is invalid"
+      flash[:login_alert] = "Username / password combination is invalid"
+      redirect_to login_path
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :tasks, :users, :projects
+  resources :tasks
+  resources :users, only: [:edit, :show, :index]
+  resources :projects
   resources :registrations, only: [:new, :create]
 
   # The priority is based upon order of creation: first created -> highest priority.
