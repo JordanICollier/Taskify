d8b7224d702557d46375471d9fa1cdb48b5bc9c0
Jordan Collier <jordanicollier@gmail.com>
2015-02-18 20:54:31 -0700

    write test for signing up/in/out user

--- spec/features/crud_users_spec.rb
+++ spec/features/crud_users_spec.rb
@@ -1,32 +1,61 @@
 require 'rails_helper'
 
-describe 'Users can CRUD tasks' do
+describe 'Signing up/in/out as user' do
 
- before :each do
-   @User = User.create(first_name: 'Jordan', last_name: 'Collier', email: "jordanicollier@gmail.com")
-   visit "/users"
-   expect(page).to have_content 'Jordan'
- end
+  before :each do
+    #  visit "/"
+    #  click_on 'Sign Up'
+    #  fill_in "First name", with: 'John'
+    #  fill_in "Last name", with: 'Doe'
+    #  fill_in "Email", with: 'j@gmail.com'
+    #  fill_in "Password", with: 'password'
+    #  fill_in "Password confirmation", with: 'password'
+    #  click_button "Sign Up"
+    #  expect(page).to have_content 'User was successfully created.'
+    #  click_on "Sign Out"
+  end
 
- it "can create a user" do
-   click_on 'New User'
-   fill_in "First name", with: 'John'
-   fill_in "Last name", with: 'Doe'
-   fill_in "Email", with: 'j@gmail.com'
-   click_button "Create User"
-   expect(page).to have_content 'User was successfully created.'
- end
+  it "can create a user" do
+    visit "/"
+    click_on 'Sign Up'
+    fill_in "First name", with: 'John'
+    fill_in "Last name", with: 'Doe'
+    fill_in "Email", with: 'j@gmail.com'
+    fill_in "Password", with: 'password'
+    fill_in "Password confirmation", with: 'password'
+    click_button "Sign Up"
+    expect(page).to have_content 'User was successfully created.'
+  end
 
- it 'can edit a user' do
-   click_on 'Edit'
-   click_on 'Update User'
-   expect(page).to have_content 'User was successfully updated.'
- end
+  it "can sign in as a user" do
+    visit "/"
+    click_on 'Sign Up'
+    fill_in "First name", with: 'John'
+    fill_in "Last name", with: 'Doe'
+    fill_in "Email", with: 'j@gmail.com'
+    fill_in "Password", with: 'password'
+    fill_in "Password confirmation", with: 'password'
+    click_button "Sign Up"
+    expect(page).to have_content 'User was successfully created.'
+    click_on "Sign Out"
 
- it 'can delete a user' do
-   click_on 'Destroy'
-   expect(page).to have_content 'User was successfully deleted.'
- end
-
-
+    visit "/login"
+    click_on 'Sign In'
+    fill_in "Email", with: 'j@gmail.com'
+    fill_in "Password", with: 'password'
+    click_button "Login!"
+    expect(page).to have_content 'Welcome back, stranger!'
+  end
+  it "can sign out as a user" do
+    visit "/"
+    click_on 'Sign Up'
+    fill_in "First name", with: 'John'
+    fill_in "Last name", with: 'Doe'
+    fill_in "Email", with: 'j@gmail.com'
+    fill_in "Password", with: 'password'
+    fill_in "Password confirmation", with: 'password'
+    click_button "Sign Up"
+    expect(page).to have_content 'User was successfully created.'
+    click_on "Sign Out"
+  end
 end
