625ddbed523159df47c0e78596bd731749688997
Jordan Collier <jordanicollier@gmail.com>
2015-02-26 14:51:28 -0700

    ruby interpolation within controller for project show view

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -30,10 +30,11 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     @project = Project.find(params[:project_id])
+    @task.project_id = params[:project_id]
 
     respond_to do |format|
       if @task.save
-        format.html { redirect_to @task, notice: 'Task was successfully created.' }
+        format.html { redirect_to project_tasks_path(@project), notice: 'Task was successfully created.' }
         format.json { render :show, status: :created, location: @task }
       else
         format.html { render :new }
