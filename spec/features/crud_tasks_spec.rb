424284d2cbdf7350c00d7e2130374c7b4d7af059
Jordan Collier <jordanicollier@gmail.com>
2015-02-27 15:22:26 -0700

    indent some files

--- spec/features/crud_tasks_spec.rb
+++ spec/features/crud_tasks_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 describe 'Users can CRUD tasks' do
 
- before :each do
-  #  @task = Task.create(description: 'Task hard bro', due_date: '08-19-2015', complete: false)
+  before :each do
+    #  @task = Task.create(description: 'Task hard bro', due_date: '08-19-2015', complete: false)
     visit "/"
     click_on 'Sign Up'
     fill_in "First name", with: 'John'
@@ -14,49 +14,49 @@ describe 'Users can CRUD tasks' do
     click_button "Sign Up"
 
 
-   click_on "Projects"
-  #  expect(page).to have_content "Task hard bro"
-  #  click_button "Projects"
-   click_on 'Create Project'
-   expect(page).to have_content "Name"
-   click_on "Create Project"
-   expect(page).to have_content "prohibited this project from being saved:"
-   fill_in "Name", with: 'turn it'
-   click_button "Create Project"
-   expect(page).to have_content 'Project was successfully created'
-   click_on '0 Tasks'
+    click_on "Projects"
+    #  expect(page).to have_content "Task hard bro"
+    #  click_button "Projects"
+    click_on 'Create Project'
+    expect(page).to have_content "Name"
+    click_on "Create Project"
+    expect(page).to have_content "prohibited this project from being saved:"
+    fill_in "Name", with: 'turn it'
+    click_button "Create Project"
+    expect(page).to have_content 'Project was successfully created'
+    click_on '0 Tasks'
 
- end
+  end
 
- it "user can create task" do
+  it "user can create task" do
 
-   click_on 'New Task'
-   expect(page).to have_content 'Description'
-   fill_in "Description", with: 'Pass the test'
-   click_button "Create Task"
-   expect(page).to have_content 'Task was successfully created.'
- end
+    click_on 'New Task'
+    expect(page).to have_content 'Description'
+    fill_in "Description", with: 'Pass the test'
+    click_button "Create Task"
+    expect(page).to have_content 'Task was successfully created.'
+  end
 
- it 'user can edit a task' do
-   click_on 'New Task'
-   expect(page).to have_content 'Description'
-   fill_in "Description", with: 'Pass the test'
-   click_button "Create Task"
-   expect(page).to have_content 'Task was successfully created.'
-   click_on 'Edit'
-   click_on 'Update Task'
-   expect(page).to have_content 'Task was successfully updated.'
- end
+  it 'user can edit a task' do
+    click_on 'New Task'
+    expect(page).to have_content 'Description'
+    fill_in "Description", with: 'Pass the test'
+    click_button "Create Task"
+    expect(page).to have_content 'Task was successfully created.'
+    click_on 'Edit'
+    click_on 'Update Task'
+    expect(page).to have_content 'Task was successfully updated.'
+  end
 
- it 'user can delete a task' do
-   click_on 'New Task'
-   expect(page).to have_content 'Description'
-   fill_in "Description", with: 'Pass the test'
-   click_button "Create Task"
-   expect(page).to have_content 'Task was successfully created.'
+  it 'user can delete a task' do
+    click_on 'New Task'
+    expect(page).to have_content 'Description'
+    fill_in "Description", with: 'Pass the test'
+    click_button "Create Task"
+    expect(page).to have_content 'Task was successfully created.'
 
-   click_on 'Destroy'
-   expect(page).to have_content 'Task was successfully destroyed.'
- end
+    click_on 'Destroy'
+    expect(page).to have_content 'Task was successfully destroyed.'
+  end
 
 end
