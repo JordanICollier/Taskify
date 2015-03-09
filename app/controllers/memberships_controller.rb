1f6f46cea9774c94425743c62e7df75874ca5890
Jordan Collier <jordanicollier@gmail.com>
2015-03-09 14:04:31 -0600

    add actions to memberships controller

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,5 +1,28 @@
 class MembershipsController < ApplicationController
 
-  
+  def index
+    @memberships = Membership.all
+    @membership = Membership.new
+  end
+
+  def new
+    @membership = Membership.new
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
 
 end
