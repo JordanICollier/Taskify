882c876380e17eec24d97abb74c297758ca7996a
Jordan Collier <jordanicollier@gmail.com>
2015-03-09 17:18:34 -0600

    scope for project_id for membership

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,7 +1,7 @@
 class Membership < ActiveRecord::Base
   belongs_to :project
   belongs_to :user
-  validates :user_id, presence: true, uniqueness: {message: "has already been added to this project."}
+  validates :user_id, presence: true, uniqueness: {scope: :project_id, message: "has already been added to this project."}
 
   enum role: {member: 0, owner: 1}
 
