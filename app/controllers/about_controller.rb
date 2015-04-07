8095256f4064a0e480042eb6dbace36800678df8
Jordan Collier <jordanicollier@gmail.com>
2015-04-06 21:45:30 -0600

    pull in project, task, and membership to about

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,4 +1,9 @@
 class AboutController < ApplicationController
   def index
+    @projects = Project.all
+    @tasks = Task.all
+    @memberships = Membership.all
+    @users = User.all
+    @comments = Comment.all
   end
 end
